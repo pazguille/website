9fc517d603b67250178ffcc5cb3c9e65549f074f
pazguille <guille87paz@gmail.com>
2011-09-25 14:05:51 -0300

    Create files

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html> 
+<!--[if IE]><![endif]--> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--> 
+<!--[if IE 7 ]>    <html lang="en"  class="no-js ie7"> <![endif]--> 
+<!--[if IE 8 ]>    <html lang="en"  class="no-js ie8"> <![endif]--> 
+<!--[if IE 9 ]>    <html lang="en"  class="no-js ie9"> <![endif]--> 
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]--> 
+	<head>
+		<meta charset="utf-8">
+		<title>Guillermo Paz</title>
+		<link href="/src/css/pazguille.css" rel="stylesheet" />
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
+		<!--link rel="shortcut icon" href="http://pazguillermo.com.ar/src/assets/favicon.ico"-->
+		<meta name="google-site-verification" content="ls5mX8y3vkVWPUJbX5L4C1QP6PUVkujZohehNVm16zQ" />
+	</head>
+	<body>
+		<header>
+			<hgroup>
+				<h1>Guillermo Paz</h1>
+				<h2>I'm a front end developer!</h2>
+				<h3>Check out these links to know about me...</h3>
+			</hgroup>
+		</header>
+		<nav>
+			<ul class="social">
+				<li><a href="http://www.linkedin.com/in/pazguillermo" id="linkedin" target="_blank">LinkedIn</a></li>
+				<li><a href="http://www.facebook.com/guillotepaz" id="facebook" target="_blank">Facebook</a></li>
+				<li><a href="http://twitter.com/pazguille" id="twitter" target="_blank">Twitter</a></li>
+				
+				<li><a href="mailto:guille87paz@gmail.com" id="email">E-mail</a></li>
+				<li><a href="https://profiles.google.com/guille87paz" id="google" target="_blank">Google</a></li>
+				<li><a href="http://www.youtube.com/pazguille" id="youtube" target="_blank">Youtube</a></li>
+				
+				<li><a href="skype:pazguille?add" id="skype">Skype</a></li>
+				<li><a href="http://www.google.com/reader/shared/guille87paz" id="rss" target="_blank">RSS</a></li>
+				<li><a href="http://www.playfire.com/pazguille" id="playfire" target="_blank">Playfire</a></li>
+				
+				<li><a href="http://gomiso.com/u/pazguille" id="miso" target="_blank">Miso</a></li>
+				<li><a href="http://www.amazon.com/gp/pdp/profile/AH7VAQR456VKZ" id="amazon" target="_blank">Amazon</a></li>
+				<li><a href="http://www.github.com/pazguille" id="github" target="_blank">GitHub</a></li>
+				
+			</ul>
+			<ul class="links">
+				<li><a href="http://www.chico-ui.com.ar/" target="_blank">chico-ui.com.ar</a></li>
+				<li><a href="http://www.wasabee.vg/" target="_blank">wasabee.vg</a></li>
+				<li><a href="http://www.weat.com.ar/" target="_blank">weat.com.ar</a></li>
+				<li><a href="http://www.spaghettiblog.com.ar/" target="_blank">spaghettiblog.com.ar</a></li>
+			</ul>
+		</nav>
+
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-15753373-2']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+	</body>
+</html>
